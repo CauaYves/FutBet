d7717c9e55a4260ce01bea36efcc42611b64ec74
Dandara <scan.dandara@voeazul.com.br>
2023-09-30 21:13:14 -0300

    fix: loadenv method realocated before imports

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,17 @@ import express, { Express } from "express"
 import { connectDb, disconnectDB } from "./config/database";
 import { loadEnv } from "./config/envs";
 import cors from "cors";
-import { betsRouter, gamesRouter, participantsRouter } from "./routes";
+import { betsRouter, gamesRouter, participantsRouter } from "./routes/index";
+loadEnv();
 
 const app = express();
 app
   .use(cors())
   .use(express.json())
+  .get("/health", (_req, res) => res.send("App running ok!"))
   .use("/participants", participantsRouter)
   .use("/games", gamesRouter)
-  .use("bets", betsRouter)
-loadEnv();
+  .use("/bets", betsRouter)
 
 export function init(): Promise<Express> {
   connectDb();
