761d93b9bf4c28b3215bccfd8a1b6604a404f385
Yves <cauayves5@gmail.com>
2023-10-02 12:30:30 -0300

    feat: all controllers completed

--- src/controllers/participants-controller.ts
+++ src/controllers/participants-controller.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { participantsService } from '@/services';
 
 export async function createParticipant(req: Request, res: Response) {
   try {
-    console.log(req.body);
-    return res.sendStatus(200);
-  } catch (error) {
-    return "controller error";
+    const result = await participantsService.createParticipant(req.body);
+    return res.status(httpStatus.CREATED).send(result);
+  } catch (err) {
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export async function getParticipants(req: Request, res: Response) {
+  try {
+    const result = await participantsService.getParticipants();
+    return res.status(result.status).send(result.data);
+  } catch (err) {
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
   }
 }
