a5be9ec1bbfd78bae1bcd53e70992f524989d5bf
Yves <cauayves5@gmail.com>
2023-10-02 12:28:10 -0300

    feat: games controller

---
+++ src/controllers/games-controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { gamesService } from '@/services';
+
+export async function createGame(req: Request, res: Response) {
+  try {
+    const result = await gamesService.createGame(req.body);
+    return res.status(httpStatus.CREATED).send(result);
+  } catch (err) {
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export async function getGames(req: Request, res: Response) {
+  try {
+    const result = await gamesService.getGames();
+    return res.status(result.status).send(result.data);
+  } catch (err) {
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export async function getGameById(req: Request, res: Response) {
+  try {
+    const result = await gamesService.getGameById(parseInt(req.params.id, 10));
+    return res.status(result.status).send(result.data);
+  } catch (err) {
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export async function finishGame(req: Request, res: Response) {
+  try {
+    const result = await gamesService.finishGame(parseInt(req.params.id, 10), req.body);
+    return res.status(httpStatus.CREATED).send(result);
+  } catch (err) {
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
